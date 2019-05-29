0a3a0f1317442edd35ebdb9517dc7488bef2f170
Daniel Ellis <ellis.daniel0@gmail.com>
2019-05-29 07:07:37 +0000

    Refactored code, take a look through old versions to see differences in TDD

--- evens.py
+++ evens.py
@@ -1,16 +1,10 @@
+def is_even(number):
+    return number % 2 == 0
+
 def even_number_of_evens(numbers):
-    if numbers == []:
-        return False
-    else:
-        evens = 0
-        
-    for n in numbers:
-        if n % 2 == 0:
-            evens += 1
-    if evens == 0:
-        return False
-    else:
-        return evens % 2 == 0
+    
+    evens = sum([1 for n in numbers if is_even(n)])
+    return False if evens == 0 else is_even(evens)
 
 assert even_number_of_evens([]) == False, "No Numbers"
 assert even_number_of_evens([2,  4]) == True, "Two even Numbers"
