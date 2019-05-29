38fe6a6b98b35f1ec9466b459b21bd3a93f69cd8
Daniel Ellis <ellis.daniel0@gmail.com>
2019-05-29 07:02:37 +0000

    TDD updated using walkthrough

--- evens.py
+++ evens.py
@@ -1,24 +1,20 @@
 def even_number_of_evens(numbers):
     if numbers == []:
         return False
-    else: 
+    else:
         evens = 0
-        for number in numbers:
-            if number % 2 == 0:
-                evens += 1
-                
+        
+    for n in numbers:
+        if n % 2 == 0:
+            evens += 1
     if evens == 0:
         return False
     else:
         return evens % 2 == 0
-    
-    
-assert even_number_of_evens([]) == False, "No Numbers"
-assert even_number_of_evens([2]) == False, "One even number"
-assert even_number_of_evens([2, 4]) == True, "two even numbers"
-assert even_number_of_evens([2, 3]) == False, "Two numbers, 1 even"
-assert even_number_of_evens([2,3,9,10,13,7,8]) == False, "Multiple odd numbers"
-assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8, 5, 12]) == True, "4 Evens, all even"
-assert even_number_of_evens([1, 3 ,9]) == False, "No Evens"
 
-print("All tests passed!") 
+assert even_number_of_evens([]) == False, "No Numbers"
+assert even_number_of_evens([2,  4]) == True, "Two even Numbers"
+assert even_number_of_evens([2]) == False, "One Even Number"
+assert even_number_of_evens([1,3,9]) == False, "Three odd nums"
+
+print("all tests Passed!")
